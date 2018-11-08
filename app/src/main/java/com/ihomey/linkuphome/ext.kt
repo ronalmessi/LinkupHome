4ac6577dc133e9f46345d568b26795e3f86e3010
董才正 <763522252@qq.com>
2018-11-08 15:51:28 +0800

    修正床头灯时间同步指令内容错误的bug

--- app/src/main/java/com/ihomey/linkuphome/ext.kt
+++ app/src/main/java/com/ihomey/linkuphome/ext.kt
@@ -373,14 +373,21 @@ fun syncTime(mac: String) {
     val calendar = Calendar.getInstance()
     calendar.time = Date()
     val year = calendar.get(Calendar.YEAR) - 2000
-    val month = calendar.get(Calendar.MONTH)
+    val month = calendar.get(Calendar.MONTH)+1
     val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
     var dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
     if (dayOfWeek == 1) dayOfWeek = 7 else {
         dayOfWeek -= 1
     }
     val hour = calendar.get(Calendar.HOUR_OF_DAY)
-    val minute = calendar.get(Calendar.MINUTE) }
+    val minute = calendar.get(Calendar.MINUTE)
+    val second = calendar.get(Calendar.SECOND)
+    val code_lawn_time_prefix = CODE_LIGHT_SYNC_TIME_BASE + (if (year >= 10) "" + year else "0$year") + (if (month >= 10) "" + month else "0$month") + (if (dayOfMonth >= 10) "" + dayOfMonth else "0$dayOfMonth") + (if (dayOfWeek >= 10) "" + dayOfWeek else "0$dayOfWeek") + (if (hour >= 10) "" + hour else "0$hour") + (if (minute >= 10) "" + minute else "0$minute") + (if (second >= 10) "" + second else "0$second")
+    val code_check = Integer.toHexString(Integer.parseInt(code_lawn_time_prefix.substring(10, 12), 16) + Integer.parseInt(code_lawn_time_prefix.substring(12, 14), 16) + Integer.parseInt(code_lawn_time_prefix.substring(14, 16), 16) + Integer.parseInt(code_lawn_time_prefix.substring(16, 18), 16) + Integer.parseInt(code_lawn_time_prefix.substring(18, 20), 16) + Integer.parseInt(code_lawn_time_prefix.substring(20, 22), 16) + Integer.parseInt(code_lawn_time_prefix.substring(22, 24), 16) + Integer.parseInt(code_lawn_time_prefix.substring(24, 26), 16) + Integer.parseInt(code_lawn_time_prefix.substring(26, 28), 16) + Integer.parseInt(code_lawn_time_prefix.substring(28, 30), 16))
+    val code_lawn_time = code_lawn_time_prefix + (if (code_check.length > 2) code_check.substring(1, code_check.length) else code_check) + "16"
+    BleManager.getInstance().write(mac, UUID_SERVICE, UUID_CHARACTERISTIC_WRITE, code_lawn_time)
+    Log.d("aa", "--" + code_lawn_time)
+}
 
 fun getShortName(type: DeviceType) =
         when (type) {
