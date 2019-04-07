dd49fb79cdf5e6d73c2fa5213ee3652e2bb22f00
董才正 <763522252@qq.com>
2019-04-08 00:04:23 +0800

    设置空间列表控件的最大高度

---
+++ app/src/main/java/com/ihomey/linkuphome/widget/MaxHeightRecyclerView.java
@@ -0,0 +1,41 @@
+package com.ihomey.linkuphome.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+import com.ihomey.linkuphome.R;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
+
+
+public class MaxHeightRecyclerView extends SwipeMenuRecyclerView {
+    private int mMaxHeight;
+
+    public MaxHeightRecyclerView(Context context) {
+        super(context);
+    }
+
+    public MaxHeightRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context, attrs);
+    }
+
+    public MaxHeightRecyclerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context, attrs);
+    }
+
+    private void initialize(Context context, AttributeSet attrs) {
+        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.MaxHeightRecyclerView);
+        mMaxHeight = arr.getLayoutDimension(R.styleable.MaxHeightRecyclerView_maxHeight, mMaxHeight);
+        arr.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mMaxHeight > 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(mMaxHeight, MeasureSpec.AT_MOST);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
