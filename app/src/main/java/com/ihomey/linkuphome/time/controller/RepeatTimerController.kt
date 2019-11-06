4e3a2d856aea15a6e3d6de24127a6c1ee36dc436
董才正 <763522252@qq.com>
2019-11-07 00:34:52 +0800

    删除无效的文件

--- app/src/main/java/com/ihomey/linkuphome/time/controller/RepeatTimerController.kt
+++
@@ -1,9 +0,0 @@
-package com.ihomey.linkuphome.time.controller
-
-import com.ihomey.linkuphome.data.entity.Device
-
-
-interface RepeatTimerController {
-
-     fun setRepeatTimer( minute: Int, hour: Int, isOpenTimer: Boolean, isOn: Boolean, repeatMode: Int)
-}
