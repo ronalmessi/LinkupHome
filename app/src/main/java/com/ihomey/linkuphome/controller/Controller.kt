d55db634735166bdbe99b9ed4ef729752def5134
董才正 <763522252@qq.com>
2019-11-21 00:58:45 +0800

    做了一些优化

--- app/src/main/java/com/ihomey/linkuphome/controller/Controller.kt
+++ app/src/main/java/com/ihomey/linkuphome/controller/Controller.kt
@@ -6,26 +6,6 @@ package com.ihomey.linkuphome.controller
  */
 abstract class Controller {
 
-    open fun setLightColor(deviceId: Int, colorValue: String) {}
-
-    open fun setLightBright(deviceId: Int, brightValue: Int) {}
-
-    open fun setLightSpeed(deviceId: Int, speedValue: Int) {}
-
-    open fun setLightColorTemperature(deviceId: Int, colorTemperature: Int) {}
-
-    open fun setLightingMode(deviceId: Int) {}
-
-    open fun setLightPowerState(deviceId: Int, powerState: Int) {}
-
-    open fun setLightScene(deviceId: Int, sceneValue: Int) {}
-
-    open fun setTimer(deviceId: Int, minuteValue: Int, isOn: Boolean) {}
-
-    open fun setRepeatTimer(deviceId: Int, minuteValue: Int, hourValue: Int, isOpenTimer: Boolean, isOn: Boolean, isRepeat: Boolean) {}
-
-    open fun syncTime(deviceId: Int) {}
-
 
     open fun setLightPowerState(deviceAddress: String?, powerState: Int) {}
 
