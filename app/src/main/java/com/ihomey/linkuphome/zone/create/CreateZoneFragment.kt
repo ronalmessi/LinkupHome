13a7281d00d84dc3ee88a1d18f99386a859d9b6f
董才正 <763522252@qq.com>
2020-03-15 23:55:11 +0800

    恢复正常功能

--- app/src/main/java/com/ihomey/linkuphome/zone/create/CreateZoneFragment.kt
+++ app/src/main/java/com/ihomey/linkuphome/zone/create/CreateZoneFragment.kt
@@ -13,9 +13,11 @@ import com.ihomey.linkuphome.data.entity.Zone
 import com.ihomey.linkuphome.data.vo.Resource
 import com.ihomey.linkuphome.data.vo.Status
 import com.ihomey.linkuphome.getIMEI
+
 import com.ihomey.linkuphome.home.HomeActivityViewModel
 import com.ihomey.linkuphome.toast
 import kotlinx.android.synthetic.main.create_zone_fragment.*
+
 
 class CreateZoneFragment : BaseFragment() {
 
@@ -27,7 +29,7 @@ class CreateZoneFragment : BaseFragment() {
     private lateinit var homeActivityViewModel: HomeActivityViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.control_c3_view, container, false)
+        return inflater.inflate(R.layout.create_zone_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -38,30 +40,30 @@ class CreateZoneFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        et_zone_name.requestFocus()
-//        et_zone_name.setSelection(et_zone_name.text.toString().trim().length)
-//        val isDefault = arguments?.getBoolean("isDefault") ?: true
-//        if (!isDefault) iv_back.visibility = View.VISIBLE
-//        btn_save.setOnClickListener { it0 ->
-//            context?.getIMEI()?.let { it1 ->
-//                createZoneViewModel.createZone(it1, et_zone_name.text.toString().trim()).observe(viewLifecycleOwner, Observer<Resource<Zone>> {
-//                    if (it?.status == Status.SUCCESS) {
-//                        hideLoadingView()
-//                        homeActivityViewModel.setCurrentZoneId(it.data?.id)
-//                        if (isDefault) {
-//                            Navigation.findNavController(it0).navigate(R.id.action_createZoneFragment_to_homeFragment)
-//                        } else {
-//                            Navigation.findNavController(it0).popBackStack()
-//                        }
-//                    } else if (it?.status == Status.ERROR) {
-//                        hideLoadingView()
-//                        it.message?.let { it2 -> activity?.toast(it2) }
-//                    } else if (it?.status == Status.LOADING) {
-//                        showLoadingView()
-//                    }
-//                })
-//            }
-//        }
-//        iv_back.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+        et_zone_name.requestFocus()
+        et_zone_name.setSelection(et_zone_name.text.toString().trim().length)
+        val isDefault = arguments?.getBoolean("isDefault") ?: true
+        if (!isDefault) iv_back.visibility = View.VISIBLE
+        btn_save.setOnClickListener { it0 ->
+            context?.getIMEI()?.let { it1 ->
+                createZoneViewModel.createZone(it1, et_zone_name.text.toString().trim()).observe(viewLifecycleOwner, Observer<Resource<Zone>> {
+                    if (it?.status == Status.SUCCESS) {
+                        hideLoadingView()
+                        homeActivityViewModel.setCurrentZoneId(it.data?.id)
+                        if (isDefault) {
+                            Navigation.findNavController(it0).navigate(R.id.action_createZoneFragment_to_homeFragment)
+                        } else {
+                            Navigation.findNavController(it0).popBackStack()
+                        }
+                    } else if (it?.status == Status.ERROR) {
+                        hideLoadingView()
+                        it.message?.let { it2 -> activity?.toast(it2) }
+                    } else if (it?.status == Status.LOADING) {
+                        showLoadingView()
+                    }
+                })
+            }
+        }
+        iv_back.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 }
